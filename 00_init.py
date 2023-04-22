42c73e7c5877bfefdf522e29099c9abc2b187f85
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2023-04-22 20:17:03 +0200

    new 00 scripts

---
+++ 00_init.py
@@ -0,0 +1,23 @@
+
+""" test with library newest version"""
+
+from PyTechBrain import *
+from time import sleep
+
+test_board = PyTechBrain()
+if test_board.board_init():
+    print("Super!")
+    test_board.set_buzzer("beep")  # demo, on, off
+    print("f", test_board.get_fotoresistor_raw())
+    print("p", test_board.get_potentiometer_scale())
+    print("c", test_board.get_temperature_celcius())
+    print("v", test_board.get_volume_sensor_raw())
+    sleep(0.1)
+    test_board.set_rgb_color((255, 255, 255))
+    test_board.set_signal_red("on")
+    test_board.set_signal_yellow("on")
+    test_board.set_signal_green("on")
+    test_board.set_pwm_diode(255)
+    print("KONIEC")
+    sleep(3)
+    test_board.set_off_outputs()
